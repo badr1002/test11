e4e2594b38f9618ac084e947264eeb16f0f67efe
Badr Helal <baderhelal567@gmail.com>
2021-11-11 15:51:47 +0200

    dd

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const fun = async (urls = [], parentBranch, childBranch) => {
     try {
         let x = Math.random()
         for (let url of urls) {
-            // await git.init();
-            // await git.addRemote(`origin${x}`, url);
-            
-            await git.checkout(['-b', `${parentBranch}`])
-            await git.commit('s1')
+            await git.init();
+            await git.addRemote(`origin`, url);
+            //await git.pull('origin','main')
+            //await git.checkout(['main'])
+            // await git.checkout(['-b', `${parentBranch}`])
+            // await git.commit('s1')
             // await git.checkoutBranch(`${childBranch}`, `${parentBranch}`)
-            // git.push(`origin${x}`, `${childBranch}`)
+             git.push(`origin${x}`, `${childBranch}`)
         }
     }
     catch (e) { console.log(e); }
@@ -21,4 +22,4 @@ const fun = async (urls = [], parentBranch, childBranch) => {
 let = url1 = 'https://github.com/badr1002/test11.git'
 let = url2 = 'https://github.com/badr1002/test1.git'
 // let = url3 = 'https://github.com/badr1002/test2.git'
-fun([url1], 'R5.6', 'child1')
+fun([url2], 'R5.5', 'child1')
